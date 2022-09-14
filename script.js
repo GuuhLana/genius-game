3d8df86bef7df0951a238bea3706c876e9a915eb
Gustavo-Notebook <gustavolr401@gmail.com>
2022-09-14 10:45:47 -0300

    Adicionando referencias iniciais

---
+++ script.js
@@ -0,0 +1,7 @@
+const countValue = document.getElementById("count");
+const colorPart = document.querySelectorAll(".color-part");
+const container = document.querySelector(".count");
+const startButton = document.querySelector("#start");
+const result = document.querySelector("#result");
+const wrapper = document.querySelector(".wrapper");
+
